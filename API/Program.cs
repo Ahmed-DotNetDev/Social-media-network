fd7f42e39ba436a0cd068fc454554153fd5c955f
Ahmed DotNet Dev <AhmedAdala@gmail.com>
2023-09-18 02:45:35 +0300

    Add client side app

--- API/Program.cs
+++ API/Program.cs
@@ -2,18 +2,18 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<DataContext>(opt=>{
+builder.Services.AddDbContext<DataContext>(opt =>
+{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseAuthorization();
 
 app.MapControllers();
