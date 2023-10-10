c5743fefa0c652193819a16c131e34de7378746c
Ahmed DotNet Dev <AhmedAdala@gmail.com>
2023-10-10 22:58:29 +0300

    End of login and register05

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private CurrentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.CurrentUserSource.asObservable();
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.CurrentUserSource.next(user);
+        }
+      })
+    )
+  }
+  
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.CurrentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentuser(user: User) {
+    this.CurrentUserSource.next(user);//update value when emmit any change happen 
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.CurrentUserSource.next(null);
+  }
+}
